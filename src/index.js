7d4ad125560f5bac8f0e56bcac27abef17da2fb7
ahlam <ahlambenali2017@gmail.com>
2018-11-07 22:22:13 +0000

    get input in json

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
 const port = 3000
 
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+
 app.post('/', function (req, res) {
-  res.send('POST request to the homepage')
+  const body = req.body
+
+  res.send(`POST request to the homepage ${JSON.stringify(body,null,2)}`)
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
