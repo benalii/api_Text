ed35531646c5cfaed79376d91e4e160b947c4cf4
ahlam <ahlambenali2017@gmail.com>
2018-11-08 01:14:29 +0000

    change number of char by line to 80  isString instaed of isNil

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const justify = require('justified');
-const { isNil, get } = require("lodash/fp")
 
+const bodyParser = require('body-parser')
+
+const justify = require('justified');
+const { isString, get } = require('lodash/fp')
+
+const app = express()
 const port = 3000
 
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 
 app.post('/', function (req, res) {
   const text = get( "body.text", req)
-  if(isNil(text)){
+  if(!isString(text)){
     res.sent({Status:500, error:"input error"})
   }else{
-   const output = justify(text,{ width:60 })
+   const output = justify(text,{ width:80 })
    res.send({
      Status:200,
      body : {
